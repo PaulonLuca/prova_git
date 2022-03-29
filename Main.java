ee702472b012023b37e9a7f9211638cc4f7361bb
Matteo Bassani <matteobassani99@gmail.com>
2022-03-29 10:16:57 +0200

    aggiunta terza stampa

--- Main.java
+++ Main.java
@@ -3,5 +3,6 @@ public class Main
     public static void main(String[] args) {
         System.out.println("Stampa");
         System.out.println("Stampa2");
+        System.out.println("Stampa3");
     }
 }
